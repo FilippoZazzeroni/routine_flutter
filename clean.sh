8aa7e4157c3f044dbef7c1863a14507e0f27db9d
Filippo Zazzeroni <filippo.zazzeroni@gmail.com>
2022-02-24 17:55:50 +0100

    Added corrected clean.sh

--- clean.sh
+++ clean.sh
@@ -1,32 +1,29 @@
 
 
 clean_project() {
+  print_current_dir
   find $PWD -type f -name "pubspec.yaml" | while read line; do
-    search_for_base_path
-  done
-  search_for_base_path
+    echo "$line"
+  done | search_for_base_path $line
 }
 
 search_for_base_path() {
-  base_path=$line
-  echo "$base_path"
-  if [ -z "$base_path" ]
+  if [ -z "$0" ]
     then
-      print_current_dir
       echo "no files switching directory"
       cd ..
-      print_current_dir
       clean_project
     else
       flutter clean
       flutter pub get
-      exit 0
-    fi
+  fi
+  exit 0
 }
 
 print_current_dir() {
   echo "current directory: $PWD"
 }
+
 
 clean_project
 
